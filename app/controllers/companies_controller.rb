56744f1d129c039e16ca93d3e4a7e6175aee1a72
ChristianLiu <christianliu@college.harvard.edu>
2014-06-23 16:01:50 +0200

    required sign_in, validations

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,6 @@
 class CompaniesController < ApplicationController
+  before_filter :authenticate_user!
+
   def new
 
   end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,7 @@
 class Company < ActiveRecord::Base
 	belongs_to :user
+
+	validates :name, presence: true
+	validates :size, presence: true
+
 end
