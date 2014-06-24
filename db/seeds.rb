2fe7a31ccce922351f0d0b9d53cfb0a6bb4bfcb0
alexbeam <beam@Alex-Beams-MacBook-Pro.local>
2014-06-24 14:13:15 +0200

    Data seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: "user1@siyelo.com", password: "12345678", name: "John Doe")
+User.create(email: "user2@siyelo.com", password: "12345678", name: "Jim Doe")
+User.create(email: "user3@siyelo.com", password: "12345678", name: "Jane Doe")
+
+c1 = Company.create(name: "Siyelo", size: 16, status: "Hiring", location: "79 Roeland Street, Cape Town, South Africa", latitude: -33.9295489, longitude: 18.4222019)
+c1.avatar = File.open('app/assets/images/seeds/siyelo.png')
+c1.save!
+
+c2 = Company.create(name: "Facebook", size: 17, status: "Hiring", location: "70 Roeland Street, Cape Town, South Africa", latitude: -34.9295489, longitude: 18.4222019)
+c2.avatar = File.open('app/assets/images/seeds/url.jpg')
+c2.save!
+
+c3 = Company.create(name: "Siyelo", size: 18, status: "Hiring", location: "82 Roeland Street, Cape Town, South Africa", latitude: -33.9295489, longitude: 19.4222019)
+c3.avatar = File.open('app/assets/images/seeds/siyelo.png')
+c3.save!
+
+c4 = Company.create(name: "Facebook", size: 19, status: "Hiring", location: "62 Roeland Street, Cape Town, South Africa", latitude: -33.9295489, longitude: 19.4222019)
+c4.avatar = File.open('app/assets/images/seeds/url.jpg')
+c4.save!
