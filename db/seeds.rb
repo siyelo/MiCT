e9358f8921873a79a323e643662fc8ea9bff5c00
alexbeam <beam@Alex-Beams-MacBook-Pro.local>
2014-07-01 14:22:56 +0200

    Search moved to home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,19 +10,19 @@ User.create(email: "user1@siyelo.com", password: "12345678", name: "John Doe")
 User.create(email: "user2@siyelo.com", password: "12345678", name: "Jim Doe")
 User.create(email: "user3@siyelo.com", password: "12345678", name: "Jane Doe")
 
-c1 = Company.create(name: "Siyelo", user_id: 6, phone: "123-456-7890", size: 16, status: "Hiring", location: "79 Roeland Street, Cape Town, South Africa", latitude: -33.9295489, longitude: 18.4222019)
+c1 = Company.create(name: "Siyelo", user_id: User.first.id, phone: "123-456-7890", size: 16, status: "Hiring", location: "79 Roeland Street, Cape Town, South Africa", latitude: -33.9295489, longitude: 18.4222019)
 c1.avatar = File.open('app/assets/images/seeds/siyelo.png')
 c1.save!
 
-c2 = Company.create(name: "Greenpop", user_id: 6, phone: "123-456-7890", size: 17, status: "Hiring", location: "70 Roeland Street, Cape Town, South Africa", latitude: -34.9295489, longitude: 18.4222019)
+c2 = Company.create(name: "Greenpop", user_id: User.first.id, phone: "123-456-7890", size: 17, status: "Hiring", location: "70 Roeland Street, Cape Town, South Africa", latitude: -34.9295489, longitude: 18.4222019)
 c2.avatar = File.open('app/assets/images/seeds/url.jpg')
 c2.save!
 
-c3 = Company.create(name: "Platform 45", user_id: 6, phone: "123-456-7890", size: 18, status: "Hiring", location: "82 Roeland Street, Cape Town, South Africa", latitude: -33.9295489, longitude: 19.4222019)
+c3 = Company.create(name: "Platform 45", user_id: User.first.id, phone: "123-456-7890", size: 18, status: "Hiring", location: "82 Roeland Street, Cape Town, South Africa", latitude: -33.9295489, longitude: 19.4222019)
 c3.avatar = File.open('app/assets/images/seeds/siyelo.png')
 c3.save!
 
-c4 = Company.create(name: "Facebook", user_id: 6, phone: "123-456-7890", size: 19, status: "Hiring", location: "62 Roeland Street, Cape Town, South Africa", latitude: -33.9295489, longitude: 19.4222019)
+c4 = Company.create(name: "Facebook", user_id: User.second.id, phone: "123-456-7890", size: 19, status: "Hiring", location: "62 Roeland Street, Cape Town, South Africa", latitude: -33.9295489, longitude: 19.4222019)
 c4.avatar = File.open('app/assets/images/seeds/url.jpg')
 c4.save!
 
