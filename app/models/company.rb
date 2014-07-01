902a27377fcea6e8198814bcdfaf1b5223790f09
alexbeam <beam@Alex-Beams-MacBook-Pro.local>
2014-07-01 13:35:28 +0200

    Small map changes with images

--- app/models/company.rb
+++ app/models/company.rb
@@ -34,7 +34,7 @@ class Company < ActiveRecord::Base
 	geocoded_by :location
 	after_validation :geocode
 
-	has_attached_file :avatar, :style => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :avatar, :content_type => /\Aimage/
 	validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/]
 
